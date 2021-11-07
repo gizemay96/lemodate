6c376eed6026490d53af33fa809a32cd672d1f7d
Gizem AY <gizem.ay1996@gmail.com>
2021-11-07 22:20:09 +0300

    init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  constructor(private translate: TranslateService, public router: Router) {
+    const lang = window.localStorage.getItem('lang');
+
+    if (lang) {
+      this.translate.use(lang)
+    }
+
+  }
+
+  changeTo(lang: string) {
+    window.localStorage.setItem('lang', lang)
+    this.translate.use(lang)
+  }
+
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HomeComponent } from './pages/home/home.component';
+
+export function HttpLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/language/', '.json')
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: window.localStorage.getItem('lang') || 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoader,
+        deps: [HttpClient]
+      },
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
